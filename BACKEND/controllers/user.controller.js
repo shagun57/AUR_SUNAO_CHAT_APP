4ab8f9789f302a706baa8495583b4363718c7d6c
shagun57 <shagunmonga57@gmail.com>
2024-04-08 12:22:15 +0530

    user controller completed

--- BACKEND/controllers/user.controller.js
+++ BACKEND/controllers/user.controller.js
@@ -1,4 +1,18 @@
-export const getUsersForSideBar = async (req,res) => {
+import User from "../models/user.model.js";
 
+export const getUsersForSideBar = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id
+
+        //find all users  except the current user(ne - not equal) and sort them.
+        const filterAllUsers = await User.find({_id: {$ne:  loggedInUserId}})
+        .select('-password')
+        .sort({username: 1})
+
+        res.status(200).json(filterAllUsers)
+    } catch (error) {
+        console.log("error in get user controller", error.message);
+        res.status(500).json({Error: "Internal Server Error"});
+    }
 }
 
